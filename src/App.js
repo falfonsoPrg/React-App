187f9e1a3a515d3504f33c8266f2e88046910312
Fabian Alfonso <falfonso@unbosque.edu.co>
2019-04-20 20:26:23 -0500

    Bug

--- src/App.js
+++ src/App.js
@@ -6,12 +6,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <Inventory />
-=======
           <Navbar />
           <Production />
->>>>>>> 69ba2c911f5cc6b8300c0f2d24eaac14f3727294
       </div>
     );
   }
